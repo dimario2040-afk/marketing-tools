160d6fd1db93f46dea82db822e56eaf7854d0a66
dimario2040-afk <dimario2040@gmail.com>
2026-05-30 00:06:08 +0300

    fix: server.js variable scoping for Railway

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 const app = require('./app');
-const { init } = require('./db/init');
-const { ensureTable } = require('./services/shortener');
 
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || '0.0.0.0';
 
-async function start() {
+async function main() {
   try {
+    const { init } = require('./db/init');
+    const { ensureTable } = require('./services/shortener');
     await init();
     await ensureTable();
     console.log('Database ready');
@@ -22,23 +22,10 @@ async function start() {
     console.log('SIGTERM received, shutting down...');
     server.close(() => process.exit(0));
   });
-
   process.on('SIGINT', () => {
     console.log('SIGINT received, shutting down...');
     server.close(() => process.exit(0));
   });
 }
 
-start();
-
-process.on('SIGTERM', () => {
-  console.log('SIGTERM received, shutting down...');
-  server.close(() => process.exit(0));
-});
-
-process.on('SIGINT', () => {
-  console.log('SIGINT received, shutting down...');
-  server.close(() => process.exit(0));
-});
-
-module.exports = server;
+main();
